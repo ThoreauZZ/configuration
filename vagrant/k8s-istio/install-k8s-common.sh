c482210f3063c9758fdd3be4631ef3dc3ee20f9a
thoreau <zz.thoreau@Gmail.com>
2018-10-07 17:15:29 +0800

    update k8s master install shell

--- vagrant/k8s-istio/install-k8s-common.sh
+++ vagrant/k8s-istio/install-k8s-common.sh
@@ -48,12 +48,7 @@ then
     mkdir -p /etc/systemd/system/docker.service.d
     cat <<EOF > /etc/systemd/system/docker.service.d/http-proxy.conf
 [Service]
-Environment="HTTP_PROXY=${HTTP_PROXY}" NO_PROXY="localhost,127.0.0.1,docker.io,registry.docker-cn.com,registry.cn-hangzhou.aliyuncs.com"
-EOF
-
-    cat <<EOF > /etc/systemd/system/docker.service.d/https-proxy.conf
-[Service]
-Environment="HTTPS_PROXY=${HTTP_PROXY}" NO_PROXY="localhost,127.0.0.1,docker.io,registry.docker-cn.com,registry.cn-hangzhou.aliyuncs.com"
+Environment="HTTP_PROXY=${HTTP_PROXY}" "HTTPS_PROXY=${HTTP_PROXY}" NO_PROXY=localhost,127.0.0.1,.docker.io,.docker-cn.com,.aliyuncs.com"
 EOF
 fi
 
